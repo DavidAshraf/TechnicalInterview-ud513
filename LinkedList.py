0b459047618ea33cde937923aafcdc764074aeb7
DavidAshraf <davida.mikhail@gmail.com>
2018-02-20 02:02:24 +0200

    Add Stack class and its testing , Modify LinkedList Class

--- LinkedList.py
+++ LinkedList.py
@@ -70,3 +70,28 @@ class LinkedList(object):
                 current.next=current.next.next
                 break
 
+    def insert_first(self, new_element):
+        "Insert new element as the head of the LinkedList"
+        new_element.next=self.head
+        self.head=new_element
+
+
+    def delete_first(self):
+        "Delete the first (head) element in the LinkedList as return it"
+        head=self.head
+        if self.head==None:
+            return None
+        self.head=self.head.next
+        return head
+
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
+        return self.ll.delete_first()
